1721a0406223a585e5f76e87063eb32a8ec1ee75
Wray Crawford <wrayhome@Wrays-iMac.local>
2019-04-15 07:39:22 -0500

    added more tests

--- spec/time_sum_spec.rb
+++ spec/time_sum_spec.rb
@@ -7,11 +7,19 @@ describe 'Adding minutes to set time' do
     expect(time_sum("9:00 AM", 15)).to eq("9:15 AM")
   end
 
-  it 'should raise error if format not valid' do
-    expect{time_sum("0:00 CM", 10)}.to raise_error(RuntimeError)
+  it 'should raise RuntimeError error if format is not a valid time' do
+    expect{time_sum("9:00 CM", 10)}.to raise_error(RuntimeError)
+  end
+
+  it 'should raise RuntimeError error if format is not a valid time' do
+    expect{time_sum("0:00 AM", 10)}.to raise_error(RuntimeError)
   end
 
   it 'should raise NoMethodError if not a time format' do
     expect{time_sum("This should fail.", 10)}.to raise_error(NoMethodError)
+  end
+
+  it 'should subtract time if negative argument' do
+    expect(time_sum("9:00 AM", -10)).to eq("8:50 AM")
   end
 end
